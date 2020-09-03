a082f9239b2238df976e5ba2cc45cf55ae6e2811
Matt Felten <m@mattfelten.com>
2020-09-03 16:40:19 -0700

    Move storybook. Make start do watching

---
+++ storybook/src/stories/emotion.stories.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { boolean, select } from '@storybook/addon-knobs';
+import { Button } from '@a/emotion';
+
+export default {
+	title: 'Emotion',
+	component: Button
+}
+
+export const button = (args) => <Button {...args} />;
+button.args = {
+	children: 'Button Label'
+}
+
+export const knobs = () => (
+	<Button
+		color={select(
+			'color',
+			['primary', 'default', 'negative'],
+			'default'
+		)}
+		disabled={boolean('disabled', false)}
+		fill={select('fill', ['solid', 'outline', 'subtle'], 'solid')}
+		loading={boolean('loading', false)}
+		size={select(
+			'size',
+			['large', 'medium', 'small', 'xsmall'],
+			'medium'
+		)}
+		full={boolean('full', false)}
+		type={select('type', ['', 'button', 'submit', 'reset'], 'button')}
+		href={select(
+			'href',
+			['', 'https://anvil-react.servicetitan.com/'],
+			''
+		)}
+		iconName={select('icon', [null, 'add', 'edit', 'more_vert'], null)}
+	>
+		{select('children', ['Button', null], 'Button')}
+	</Button>
+);
